04365c67e240e082d3a5a91f37533c6a6c9be1fe
Aaron Lazaro <190507716+Aaronicss@users.noreply.github.com>
2026-07-20 01:13:56 +0800

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+/* Generate 4 random integers and create an array */
+app.get("/api/quiz", async (req, res) => {
+  const pokemonFirstGen = 151;
+  const uniqueNumbers = new Set();
+  while (uniqueNumbers.size < 4) {
+    const num = Math.floor(Math.random() * pokemonFirstGen) + 1;
+    uniqueNumbers.add(num);
+  }
+  const idArray = Array.from(uniqueNumbers);
+  console.log("Generated IDs:", idArray);
+
+  /* After creating the ID arrays, create a promise then fetch the data */
+
+  const pokemonPromises = idArray.map((id) =>
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json()),
+  );
+
+  /* Wait for all the data to be fetched */
+
+  const pokemonData = await Promise.all(pokemonPromises);
+
+  console.log("First Pokemon Name:", pokemonData[0].name);
+
+  const options = pokemonData.map((p) => {
+    const cleanName = p.name.charAt(0).toUpperCase() + p.name.slice(1);
+    return { id: p.id, name: cleanName };
+  });
+
+  const correctPokemon = pokemonData[Math.floor(Math.random() * 4)];
+
+  res.json({
+    image: correctPokemon.sprites.other["official-artwork"].front_default,
+    correctId: correctPokemon.id,
+    options: options,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,91 @@
+import pika from "./assets/pika.png";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [quizData, setQuizData] = useState(null);
+  const [answer, setAnswer] = useState("");
+  const [score, setScore] = useState(0);
+
+  const fetchNewQuestion = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/quiz");
+      const data = await response.json();
+      setQuizData(data);
+    } catch (error) {
+      console.error("Error connecting to backend:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNewQuestion();
+  }, []);
+
+  const correctAns = () => {
+    setAnswer(true);
+    incrementScore();
+  };
+
+  const wrongAns = () => {
+    setAnswer(false);
+    handleResult();
+  };
+
+  const handleGuess = (clickedId) => {
+    if (clickedId === quizData.correctId) {
+      setAnswer(true);
+      incrementScore();
+    } else {
+      setAnswer(false);
+    }
+  };
+
+  const handleResult = () => {
+    if (answer === true) {
+      return <span className="result-text">Correct!</span>;
+    }
+    if (answer === false) {
+      return <span className="result-text">Wrong!</span>;
+    }
+  };
+
+  const incrementScore = () => {
+    const newScore = score + 1;
+    setScore(newScore);
+  };
+
+  console.log("Current Quiz Data:", quizData);
+
+  if (!quizData) {
+    return (
+      <div className="container">
+        <h1>Loading pokemon...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h1>Who's That Pokemon</h1>
+      <img src={quizData.image} alt="" className="pokemon" />
+      <div className="btn-group-selection">
+        {quizData.options.map((options) => (
+          <button
+            key={options.id}
+            className="btn-selection"
+            onClick={() => handleGuess(options.id)}
+          >
+            {options.name}
+          </button>
+        ))}
+      </div>
+
+      <div className="score">
+        <div className="show-result">{handleResult()}</div>
+        <span className="score-text">Score: {score}</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
